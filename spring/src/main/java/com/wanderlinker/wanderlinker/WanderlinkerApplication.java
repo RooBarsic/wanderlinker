ef80acbd1d9550420b52ce3aa0bc4115fa2377cd
root <root@liquididea.localdomain>
2020-06-12 19:18:37 +0200

    setup

---
+++ spring/src/main/java/com/wanderlinker/wanderlinker/WanderlinkerApplication.java
@@ -0,0 +1,13 @@
+package com.wanderlinker.wanderlinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderlinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderlinkerApplication.class, args);
+	}
+
+}
