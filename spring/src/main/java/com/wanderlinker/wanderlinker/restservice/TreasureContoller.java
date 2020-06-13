f15b4cf2d38c98816c1bd1c761038aae86b31ea8
root <root@liquididea.localdomain>
2020-06-13 16:57:55 +0200

    add postgress and first example

---
+++ spring/src/main/java/com/wanderlinker/wanderlinker/restservice/TreasureContoller.java
@@ -0,0 +1,19 @@
+package com.wanderlinker.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TreasureContoller {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/teasure")
+	public Treasure treasure(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Treasure(counter.incrementAndGet(), String.format(template, name));
+	}
+}
