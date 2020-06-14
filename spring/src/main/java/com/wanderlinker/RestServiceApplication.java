854352993c82539289badb6056fb0cc9d19b997a
Farrukh Karimov <Karimov.farrux@list.ru>
2020-06-15 02:48:49 +0300

    add COVID-19 updates

--- spring/src/main/java/com/wanderlinker/RestServiceApplication.java
+++ spring/src/main/java/com/wanderlinker/RestServiceApplication.java
@@ -1,4 +1,4 @@
-package com.wanderlinker.restservice;
+package com.wanderlinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
