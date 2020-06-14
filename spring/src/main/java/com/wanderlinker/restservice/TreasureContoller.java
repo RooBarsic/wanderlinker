79f068912bb6334a0afc589203db02b9ae525ddf
root <root@liquididea.localdomain>
2020-06-14 07:00:49 +0200

    easy rest route

--- spring/src/main/java/com/wanderlinker/restservice/TreasureContoller.java
+++ spring/src/main/java/com/wanderlinker/restservice/TreasureContoller.java
@@ -12,7 +12,7 @@ public class TreasureContoller {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/teasure")
+	@GetMapping("/treasure")
 	public Treasure treasure(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Treasure(counter.incrementAndGet(), String.format(template, name));
 	}
